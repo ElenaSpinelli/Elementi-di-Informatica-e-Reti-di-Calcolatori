1646376830f18965c21b3d7674c54e2176c27772
ElenaSpinelli <32271703+ElenaSpinelli@users.noreply.github.com>
2018-06-19 05:57:58 +0200

    d

---
+++ sistemasolare.js
@@ -0,0 +1,24 @@
+var stato = 0;
+var tween = TweenMax.to("#q", 2, {
+  rotation: "+=360", ease:Linear.easeNone,
+  repeat: -1});
+
+var tween = TweenMax.to("#p", 5, {
+  rotation: "-=360", ease:Linear.easeNone,
+  repeat: -1});
+
+$("#clikv").on("click", () => {
+ TweenMax.to("#q", 5, {
+  rotation: "-=360", ease:Linear.easeNone,
+  repeat: -1});
+})
+
+$("#clikm").on("click", () => {
+ TweenMax.to("#p", 2, {
+  rotation: "+=360", ease:Linear.easeNone,
+  repeat: -1});
+})
+
+$("#cliks").on("click", () => {
+ TweenMax.to("#s", 3,{css:{opacity:100}, ease:Quad.easeInOut});
+})
